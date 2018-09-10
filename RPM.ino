c6482f2e72b40b44b9a37f738222c5038d1d16f6
Mohamed Hassanin <pop@yahoo.com>
2018-09-10 13:15:47 +0200

    Make it print every one second

--- RPM.ino
+++ RPM.ino
@@ -14,32 +14,34 @@ LiquidCrystal lcd(A1, A0, 2, 3, 4, 5);
 
 
 volatile int state = 1;
-volatile float rpm;
+volatile int printLCD = 0;
+volatile float rpm =0;
 volatile int overflows =0;
 volatile int overflowsDet =0;
+volatile int counterR =0;
+volatile int counterEYE =0;
+
 void init_Timer1(void){
-  TCCR1B &=~(1 << ICES1);              //falling
+  TCCR1B |= (1 << ICES1); //triggers rising edge
   TIMSK1 |= 1 << ICIE1 | 1 << TOIE1;
-  TCCR1B |= 1 << CS10 | 1 << CS12; //1024
+  TCCR1B |= 1 << CS10 | 1 << CS12; // prescaler = 1024
 }
 
 ISR(TIMER1_OVF_vect){
   overflows++;
 }
 
-
-
 ISR(TIMER1_CAPT_vect){
   if(state == 1){
   TCNT1 = 0;
   overflows =0;
-  //TCCR1B |= (1 << ICES1); //rising
   state =0;
   }
   else{
   state=1;
-  //TCCR1B &=~(1 << ICES1);
+  printLCD= 1;
   overflowsDet=overflows;
+  counterR++;
   }
 }
 
@@ -54,20 +56,24 @@ int main(void)
   lcd.print("SHELL E MARATHON");
   // Turn off the blinking cursor:
   lcd.noBlink();
-  
-  PORTB |=(1 << PINB0); //pull up
+  PORTB &= ~(1 << PINB0); //pull down
   init_Timer1();
-  
   sei();
   
-    while (1) 
-    {
-      if(state == 1)
+while (1) 
+{
+  if(printLCD == 1){
   rpm =  (937500.0/(ICR1 + (overflowsDet * 65536)));
-      lcd.setCursor(0,1);
-  lcd.print(rpm);
+  counterEYE = rpm/60;  //60 >> prints every second 1Hz = 60 RPM
+  if (counterR >=counterEYE){
+  counterR=0; 
+  lcd.setCursor(0,1);
+  lcd.print(rpm * 2);  //*2 because of LCD halves the value
   lcd.print(" RPM               ");
-    }
+  printLCD=0;
+}
+}
+}
 }
 
 
