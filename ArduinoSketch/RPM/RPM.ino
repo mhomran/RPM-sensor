ec1e0bed5760c8c218d9501a96a97f8ded1c2655
Mohamed Hassanin <mohamed.pop871@yahoo.com>
2019-03-25 01:49:00 +0200

    Add ArduinoMega Sketch
    Measure the speed of the wheel and the RPM of the engine using
    ArduinoMega then sends the values to an android app using bluetooth.
    Add an app inventor project and a SDK

--- ArduinoSketch/RPM/RPM.ino
+++ ArduinoSketch/RPM/RPM.ino
@@ -1,81 +1,129 @@
   /*
- * RPM.c
+ * rpmSpeed.c
  *
  * Created: 9/3/2018 8:36:28 PM
  * Author : Mohamed Hassanin
  */ 
-#include <LiquidCrystal.h>
 
-LiquidCrystal lcd(A1, A0, 2, 3, 4, 5);
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #define F_CPU 16000000UL
 
 
-volatile int state = 1;
-volatile int printLCD = 0;
-volatile float rpm =0;
-volatile int overflows =0;
-volatile int overflowsDet =0;
-volatile int counterR =0;
-volatile int counterEYE =0;
+volatile int stateSpeed = 1;
+volatile int printSpeed = 0;
+volatile int rpmSpeed =0;
+volatile int wheelSpeed=0;
+volatile int counterEyeSpeedTemp =0;
+volatile int counterEyeSpeed =0;
+volatile int OF_Speed = 0;
 
-void init_Timer1(void){
-  TCCR1B |= (1 << ICES1); //triggers rising edge
-  TIMSK1 |= 1 << ICIE1 | 1 << TOIE1;
-  TCCR1B |= 1 << CS10 | 1 << CS12; // prescaler = 1024
-}
+volatile int stateRpmEngine = 1;
+volatile int printRpmEngine = 0;
+volatile int rpmEngine =0;
+volatile int counterEyeRpmEngineTemp =0;
+volatile int counterEyeRpmEngine =0;
+volatile int OF_RpmEngine = 0;
 
-ISR(TIMER1_OVF_vect){
-  lcd.setCursor(0,1);
-  lcd.print("                    ");
-}
+//prototypes
+void init_Timer4(void);
+void init_Timer5(void);
 
-ISR(TIMER1_CAPT_vect){
-  if(state == 1){
-  TCNT1 = 0;
-  overflows =0;
-  state =0;
-  }
-  else{
-  state=1;
-  printLCD= 1;
-  overflowsDet=overflows;
-  counterR++;
-  }
-}
-
+//------------------------------------------------------------------------------------------------------------//    
+//main
 int main(void)
 {
-  DDRD = 0b00111100;
-  DDRC |= 1 << PINC0 | 1 << PINC1;
-  
-  // set up the LCD's number of columns and rows: 
-  lcd.begin(16, 2);
-  // Print a message to the LCD.
-  lcd.print("SHELL E MARATHON");
-  // Turn off the blinking cursor:
-  lcd.noBlink();
-  PORTB &= ~(1 << PINB0); //pull down
-  init_Timer1();
-  sei();
-  
-while (1) 
-{
-  if(printLCD == 1){
-  rpm =  (937500.0/(ICR1 + (overflowsDet * 65536)));
-  counterEYE = rpm/60;  //60 >> prints every second 1Hz = 60 RPM
-  if (counterR >=counterEYE){
-  counterR=0; 
-  lcd.setCursor(0,1);
-  lcd.print(rpm * 2);  //*2 because of LCD halves the value
-  lcd.print(" RPM               ");
-  printLCD=0;
+    Serial.begin(9600);
+
+    init_Timer4();
+    init_Timer5();
+    sei();
+
+    while (1) 
+    {
+        if(printSpeed == 1){
+            rpmSpeed =  (1875000.0/(ICR4));
+            counterEyeSpeed = rpmSpeed/240;  //60 >> prints every second 1Hz = 60 rpmSpeed
+            if (counterEyeSpeedTemp >=counterEyeSpeed){
+                counterEyeSpeedTemp=0; 
+                wheelSpeed =  ( (2 * 3.14159 * (0.5664/2)) /(60.0/rpmSpeed ) ) * (18.0 /5);
+            if(wheelSpeed != -2727){                 //Garbage 
+                Serial.print('k');              //A flag for the app
+                Serial.print(wheelSpeed);
+            }
+            printSpeed=0;
+            }
+        }
+        if(printRpmEngine == 1){
+            rpmEngine =  (1875000.0/(ICR5));
+            counterEyeRpmEngine = rpmEngine/240;  //60 >> prints every second 1Hz = 60 rpmSpeed
+            if (counterEyeRpmEngineTemp >=counterEyeRpmEngine){
+                counterEyeRpmEngineTemp=0;
+                Serial.print('r');                    //A flag for the app
+                Serial.print(rpmEngine);
+                printRpmEngine=0;
+            }
+        } 
+
+        //overFlows handlers
+        if(OF_Speed){
+            Serial.print('k');
+            Serial.print('0');
+        }
+        if(OF_RpmEngine){
+            Serial.print('r');
+            Serial.print('0'); 
+        }
+    }
+}   //end of main
+
+///////////////////////////////////////////////////////////////////////////////
+//this timer is for engine RPM
+void init_Timer4(void){
+    TCCR4B |= (1 << ICES4); //triggers rising edge
+    TIMSK4 |= 1 << ICIE4 | 1 << TOIE4;
+    TCCR4B |= 1 << CS40 | 1 << CS42; // prescaler = 1024
 }
+
+ISR(TIMER4_OVF_vect){
+    OF_Speed = 1;
 }
+
+ISR(TIMER4_CAPT_vect){
+    if(stateSpeed == 1){
+        TCNT4 = 0;
+        stateSpeed =0;
+    }
+    else{
+        stateSpeed=1;
+        printSpeed= 1;
+        counterEyeRpmEngineTemp++;
+    }
 }
+//////////////////////////////////////////////////////////////////////////////
+void init_Timer5(void){
+    TCCR5B |= (1 << ICES5); //triggers rising edge
+    TIMSK5 |= 1 << ICIE5 | 1 << TOIE5;
+    TCCR5B |= 1 << CS50 | 1 << CS52; // prescaler = 1024
 }
+
+ISR(TIMER5_OVF_vect){
+    OF_RpmEngine = 1;
+}
+
+ISR(TIMER5_CAPT_vect){
+    if(stateRpmEngine == 1){
+        TCNT5 = 0;
+        stateRpmEngine =0;
+    }
+    else{
+        stateRpmEngine=1;
+        printRpmEngine= 1;
+        counterEyeSpeedTemp++;
+    }
+}
+//////////////////////////////////////////////////////////////////////////////
 
 
 
